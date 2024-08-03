b78dfe03de7b43eafb86e6bed4449c661d28951f
mohammadribhee <mohammadribhee@gmail.com>
2024-08-03 11:17:02 +0300

    Filter games by genre

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,7 +1,5 @@
-import { useState, useEffect } from "react";
-import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
 import UseData from "./useData";
+import { Genre } from "./useGenres";
 
 export interface Platform {
   id: number;
@@ -17,5 +15,8 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = () => UseData<Game>("/games");
+const useGames = (selectedGenre: Genre | null) =>
+  UseData<Game>("/games", { params: { genres: selectedGenre?.id } }, [
+    selectedGenre?.id,
+  ]);
 export default useGames;
