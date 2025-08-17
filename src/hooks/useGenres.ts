71bb92c673b7eec31b42ea99906d5d181d5fce1f
mohammadmansour814 <mohammadadmobonunity@gmail.com>
2025-08-17 15:06:46 +0300

    fix the issue

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import apiClient from "../services/api-client";
-import { FetchRespnese } from "./useData";
+import apiClient, { FetchRespnese } from "../services/api-client";
 import genres from "../data/genres";
 
 export interface Genre {
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,5 @@
 import { useQuery } from "@tanstack/react-query";
-import { FetchRespnese } from "./useData";
-import apiClient from "../services/api-client";
+import apiClient, { FetchRespnese } from "../services/api-client";
 import platforms from "../data/platforms";
 
 interface Platform {
